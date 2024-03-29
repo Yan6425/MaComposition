f54483106cb1956600f0de7d7c0277b6d2d21f08
Yan <yan.hourdebaigt.hiroux@gmail.com>
2024-03-29 13:20:09 +0100

    fini

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ ajouterCartes("femmes");
 
 function formation(choix){
   let terrain = document.getElementById("terrain");
-  terrain.innerHTML = "<div id='gardien'><div class='joueur'></div></div>";
+  let gardien = document.createElement("div");
+  gardien.classList.add("joueur");
+  gardien.addEventListener("click", placer);
+  let ligneGardien = document.createElement("div");
+  ligneGardien.id = "gardien";
+  ligneGardien.append(gardien);
+  terrain.append(ligneGardien);
   for (let i = 0; i < 3; i++){
     let ligne = document.createElement("div");
     ligne.classList.add("ligneJoueurs");
@@ -13,6 +19,7 @@ function formation(choix){
     for (let j = 0; j < nombreJoueurs; j++){
       let joueur = document.createElement("div");
       joueur.classList.add("joueur");
+      joueur.addEventListener("click", placer);
       ligne.appendChild(joueur);
     }
     ligne.appendChild(document.createElement("br"));
@@ -29,11 +36,46 @@ function ajouterCartes(genre){
     for (const [key, value] of Object.entries(joueurs)){
       let carte = document.createElement("div");
       carte.classList.add("joueur");
+      carte.addEventListener("click", selectionner);
       carte.style.backgroundImage = "url(" + value + ")";
-      carte.id = key;
       document.getElementById("listeJoueurs").appendChild(carte);
     }
   })
   .catch(error => console.error('Error fetching JSON:', error));
 }
 
+function selectionner(e){
+  let choix = document.getElementById("selection");
+  if (choix != null) {
+    choix.id = "";  
+  }
+  if (choix != e.currentTarget){
+    e.currentTarget.id = "selection";
+  }
+}
+
+function placer(e){
+  let choix = document.getElementById("selection");
+  if (choix != null){
+    choix.id = "";
+    e.currentTarget.replaceWith(choix);
+    choix.removeEventListener("click", selectionner);
+    choix.addEventListener("click", enlever);
+  }
+}
+
+function enlever(e){
+  e.currentTarget.removeEventListener("click", enlever);
+  let clone = e.currentTarget.cloneNode();
+  clone.addEventListener("click", selectionner);
+  document.getElementById("listeJoueurs").appendChild(clone);
+  let carteVierge = document.createElement("div");
+  carteVierge.classList.add("joueur");
+  carteVierge.addEventListener("click", placer);
+  e.currentTarget.replaceWith(carteVierge);
+}
+
+function valider(){
+  sessionStorage.setItem("terrain", document.getElementById("terrain").innerHTML);
+  window.location.href = "termine.html";
+}
